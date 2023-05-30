fa1b09a7e9390f93f90ba33692e2d03969577558
gabuh <nogueira.santos@aluno.ifsp.edu.br>
2023-05-29 22:44:24 -0300

    feat: packages names

--- src/main/java/br/ifsp/edu/lp1p2/HelloApplication.java
+++ src/main/java/br/ifsp/edu/lp1p2/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/hello-view.fxml"));
         Screen screen = Screen.getPrimary();
         Scene scene = new Scene(fxmlLoader.load(),screen.getBounds().getWidth()-100 , screen.getBounds().getHeight()-100);
         stage.setTitle("Pedro Vacilao!");
